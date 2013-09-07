4fce6855ee5f5567285e1f2a9fc6d2cd87a04069
DTrejo <david.daniel.trejo@gmail.com>
2013-09-06 20:56:29 -0700

    improved readme detection, font size, homepage, error page

--- readmetree.js
+++ readmetree.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var path = require('path')
 var http = require('http')
+var exec = require('child_process').exec
 
 var _ = require('underscore')
 var marked = require('marked')
@@ -14,32 +15,40 @@ if (!root) root = process.cwd()
 root = root.replace('~', process.env.HOME)
 root = path.resolve(process.cwd(), root)
 
-var content = {}
+var content = { '/' : style
+    + '<br><h1><a href="http://github.com/dtrejo/readmetree">readmetree</a><h2>'
+}
 http.createServer(function(req, res) {
-    if (!content.hasOwnProperty(req.url)) return res.end('404z dude.')
+    if (!content.hasOwnProperty(req.url)) {
+        return res.end('<h1>404z dude.</h1>'+content['/'])
+    }
     res.setHeader('content-type', 'text/html')
     return res.end(content[req.url])
-}).listen(PORT);
+}).listen(PORT)
 
 readdirRecur(root, isNodeModules)
     .filter(isReadme)
     .sort()
     .forEach(function(readme) {
+        console.log(readme)
         var shortpath = readme.replace(path.dirname(path.dirname(readme)), '')
         content[shortpath] =
             style + nav + marked(fs.readFileSync(readme, 'utf8'))
         content['/'] +=
-            '<br><a href="http://localhost:'+PORT+shortpath+'">"'
-            + 'http://localhost:'+PORT+shortpath+'</a>';
+            '<br><a href="http://localhost:'+PORT+shortpath+'">'
+            + 'http://localhost:'+PORT+'<b>'+shortpath+'</b></a>'
     })
-console.log('http://localhost:'+PORT+' is serving all your readmes')
+
+var url = 'http://localhost:'+PORT
+console.log(url+' is serving all your readmes')
+exec('open '+url)
 
 function isNodeModules(f) {
     var bname = path.basename(f)
     return 'node_modules' === bname
 }
 function isReadme(file) {
-    return path.basename(file).toLowerCase().indexOf('readme') === 0
+    return path.basename(file).toLowerCase().indexOf('readme.m') === 0
 }
 
 function readdirRecur(dir, recurAgain) {
