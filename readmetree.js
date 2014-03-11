37e5c3e2395e7c1042129e52b7602118cf28d68c
DTrejo <david.daniel.trejo@gmail.com>
2014-03-10 23:53:17 -0700

    2-space indents; spaces around plus signs

--- readmetree.js
+++ readmetree.js
@@ -16,63 +16,63 @@ if (!root) root = process.cwd()
 root = root.replace('~', process.env.HOME)
 root = path.resolve(process.cwd(), root)
 
-var content = { '/' : style
+var content = {
+  '/': style
     + '<br><h1><a href="http://github.com/dtrejo/readmetree">readmetree</a><h3>'
 }
 http.createServer(function(req, res) {
     if (!content.hasOwnProperty(req.url)) {
-        return res.end('<h1>404z dude.</h1>'+content['/'])
+      return res.end('<h1>404z dude.</h1>' + content['/'])
     }
     res.setHeader('content-type', 'text/html')
     return res.end(content[req.url])
 }).listen(PORT)
 
 readdirRecur(root, isNodeModules)
-    .filter(isReadme)
-    .sort(function(a, b) {
-        var aa = path.basename(path.dirname(a)).toLowerCase()
-        var bb = path.basename(path.dirname(b)).toLowerCase()
-        console.log(aa, bb)
-        return aa.localeCompare(bb)
-    })
-    .forEach(function(readme) {
-        // console.log(readme)
-        var shortpath = readme.replace(path.dirname(path.dirname(readme)), '')
-        if (content[shortpath]) return
-        content[shortpath] =
-            style + nav + special(marked(fs.readFileSync(readme, 'utf8')))
-        content['/'] +=
-            '<br><a href="http://localhost:'+PORT+shortpath+'">'
-            + 'http://localhost:'+PORT+'<b>'+shortpath+'</b></a>'
-    })
+  .filter(isReadme)
+  .sort(function(a, b) {
+    var aa = path.basename(path.dirname(a)).toLowerCase()
+    var bb = path.basename(path.dirname(b)).toLowerCase()
+    return aa.localeCompare(bb)
+  })
+  .forEach(function(readme) {
+    console.log(readme)
+    var shortpath = readme.replace(path.dirname(path.dirname(readme)), '')
+    if (content[shortpath]) return
+    content[shortpath] =
+      style + nav + special(marked(fs.readFileSync(readme, 'utf8')))
+    content['/'] +=
+      '<br><a href="http://localhost:' + PORT + shortpath + '">'
+      + 'http://localhost:' + PORT + '<b>' + shortpath + '</b></a>'
+  })
 
-var url = 'http://localhost:'+PORT
-console.log(url+' is serving all your readmes')
-exec('open '+url)
+var url = 'http://localhost:' + PORT
+console.log(url + ' is serving all your readmes')
+exec('open ' + url)
 
 function isNodeModules(f) {
-    var bname = path.basename(f)
-    return 'node_modules' === bname
+  var bname = path.basename(f)
+  return 'node_modules' === bname
 }
 function isReadme(file) {
-    return path.basename(file).toLowerCase().indexOf('readme.m') === 0
+  return path.basename(file).toLowerCase().indexOf('readme.m') === 0
 }
 
 function readdirRecur(dir, recurAgain) {
-    var files = fs.readdirSync(dir)
-    files = files.map(function(f) {
-        return path.join(dir, f)
+  var files = fs.readdirSync(dir)
+  files = files.map(function(f) {
+    return path.join(dir, f)
+  })
+  var moreFiles = []
+  files.forEach(function(f) {
+    if (!recurAgain(f)) return
+    // console.log('recur on', f)
+    var module_dirs = fs.readdirSync(f)
+    module_dirs.forEach(function(module_dir) {
+      moreFiles = moreFiles.concat(
+        readdirRecur(path.join(f, module_dir), recurAgain)
+      )
     })
-    var moreFiles = []
-    files.forEach(function(f) {
-        if (!recurAgain(f)) return
-        // console.log('recur on', f)
-        var module_dirs = fs.readdirSync(f)
-        module_dirs.forEach(function(module_dir) {
-            moreFiles = moreFiles.concat(
-                readdirRecur(path.join(f, module_dir), recurAgain)
-            )
-        })
-    })
-    return files.concat(moreFiles)
+  })
+  return files.concat(moreFiles)
 }
